5879482e6d6074bd80931d63209864d9b3881eaa
username1103 <username1103@naver.com>
2021-03-15 12:43:10 +0900

    JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Food({ favorite }) {
+  return <h3>나는 {favorite}를 좋아해</h3>;
+}
 
 function App() {
   return (
     <div>
       <h1>안녕!</h1>
-      <Potato />
+      <Food favorite="김치" />
+      <Food favorite="라면" />
+      <Food favorite="삼겹살" />
+      <Food favorite="쭈꾸미" />
     </div>
   );
 }

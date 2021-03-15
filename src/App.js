6bb8ac640f400ce5af2a3d6fc0904ca2bbce8780
username1103 <username1103@naver.com>
2021-03-15 12:55:39 +0900

    동적으로 컴포넌트 변경

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React from "react";
 
-function Food({ favorite }) {
-  return <h3>나는 {favorite}를 좋아해</h3>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h3>나는 {name}를 좋아해</h3>
+      <img src={picture} />
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: "김치",
+    url:
+      "https://image.shutterstock.com/image-photo/korea-food-top-view-chinese-600w-1755822044.jpg",
+  },
+  {
+    name: "라면",
+    url:
+      "https://image.shutterstock.com/image-photo/steam-smoke-instant-noodles-bowl-600w-1654396696.jpg",
+  },
+  {
+    name: "삼겹살",
+    url:
+      "https://image.shutterstock.com/image-photo/raw-pork-belly-isolated-on-600w-1595467555.jpg",
+  },
+];
 
 function App() {
   return (
     <div>
       <h1>안녕!</h1>
-      <Food favorite="김치" />
-      <Food favorite="라면" />
-      <Food favorite="삼겹살" />
-      <Food favorite="쭈꾸미" />
+      {foodILike.map((dish) => {
+        return <Food name={dish.name} picture={dish.url} />;
+      })}
     </div>
   );
 }
